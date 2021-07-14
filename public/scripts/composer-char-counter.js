b7fa2a95b14fcf4b203e73a97d9510d74da26b47
Shaun <s-purslow@hotmail.com>
2021-07-14 22:27:39 +0000

    create some error functions and clean up code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,13 @@ $(document).ready(function() {
   $('#tweet-text').keydown(function() {
     const tweetLength = $(this).val().length;
     $('#counter').text(140 - tweetLength);
+    // if characters are more that 140
     if (tweetLength > 140) {
       $('#counter').addClass('colorCounter');
+      $('.errorSection').slideDown(1000).css('display', 'block');
     } else {
       $('#counter').removeClass('colorCounter');
+      $('.errorSection').css('display', 'none');
     }
   });
 });
