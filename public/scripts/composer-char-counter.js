ac4566675863542be54e5cbb81961d9399bfb311
Shaun <s-purslow@hotmail.com>
2021-07-15 18:09:05 +0000

    fix bug in character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $('#tweet-text').keydown(function() {
-    const tweetLength = $(this).val().length;
-    $('#counter').text(140 - tweetLength);
+  $('#tweet-text').on('input', function() {
+    const maxLength = 140;
+    let tweetLength = $(this).val().length;
+    $(this).siblings('div').children('#counter').text(maxLength - tweetLength);
     // if characters are more that 140
-    if (tweetLength > 140) {
+    if (tweetLength > maxLength) {
       $('#counter').addClass('colorCounter');
       $('.errorSection').slideDown(1000).css('display', 'block');
     } else {
